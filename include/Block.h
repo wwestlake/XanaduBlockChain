4b63d537b7d3b88c06b7772b17379212a5ce67f9
wwestlake <wwestlake@lagdaemon.com>
2018-08-23 10:53:51 -0400

    Update Block.h

--- include/Block.h
+++ include/Block.h
@@ -33,27 +33,27 @@
 template <typename Tpayload>
 class Block
 {
-	shared_ptr<Block<Tpayload>> previousBlock;	//!< The previous block in the chain, null if this is the genesis block.
-	shared_ptr<Block<Tpayload>> nextBlock;		//!< The next block in the chain, null if this is the last block.
-	long blockId;					//!< The unique identifier of this block in the chain.
-	Tpayload payload; 				//!< The payload of this block represents the information that is chained.
-	boost::date date_time_stamp;			//!< The data time stamp when this was created.  Should always be recorded in UTC (coordinated universal time)
-	long nonce;					//!< Used to provide proof of work on block chains.
-	std::string previousHash; 			//!< The hash of the previous block in this chain.
-	std::string hash;				//!< The hash of a string representation of the blockId, payload, date_time_stamp, nonce, and previousHash concatenated together.
+	shared_ptr<Block<Tpayload>> previousBlock;		//!< The previous block in the chain, null if this is the genesis block.
+	shared_ptr<Block<Tpayload>> nextBlock;			//!< The next block in the chain, null if this is the last block.
+	long blockId;						//!< The unique identifier of this block in the chain.
+	Tpayload payload; 					//!< The payload of this block represents the information that is chained.
+	boost::date date_time_stamp;				//!< The data time stamp when this was created.  Should always be recorded in UTC (coordinated universal time)
+	long nonce;						//!< Used to provide proof of work on block chains.
+	std::string previousHash; 				//!< The hash of the previous block in this chain.
+	std::string hash;					//!< The hash of a string representation of the blockId, payload, date_time_stamp, nonce, and previousHash concatenated together.
 	
 public:
 	Block();
-	shared_ptr<Block<Tpayload>> getLastBlock();	//!< Gets the last block in the chain
-	shared_ptr<Block<Tpayload>> getGenesisBlock();  //!< Gets the first block (genesis block) in the chain
-	long getNextId()				//!< Gets the next ID value for this chain
+	shared_ptr<Block<Tpayload>> getLastBlock();		//!< Gets the last block in the chain
+	shared_ptr<Block<Tpayload>> getGenesisBlock();  	//!< Gets the first block (genesis block) in the chain
+	long getNextId()					//!< Gets the next ID value for this chain
 	{
 		return getLastBlock()->blockId + 1;
 	}
-	hashBlock();					//!< Sets the hash values for this block, throws an exception if the block is already hashed and part of the chain
-	verifyBlock();					//!< Verifies the hashes of this blocl
-	verifyNonceSolution();				//!< Verifies that the nonce setting on this block meets the criteria
-	shared_ptr<Block<Tpayload>> solveNonce(long newNonce); //!< Performs the nonce calulation for a specified nonce value, can only be called on new blocks
+	hashBlock();						//!< Sets the hash values for this block, throws an exception if the block is already hashed and part of the chain
+	verifyBlock();						//!< Verifies the hashes of this blocl
+	verifyNonceSolution();					//!< Verifies that the nonce setting on this block meets the criteria
+	shared_ptr<Block<Tpayload>> solveNonce(long newNonce); 	//!< Performs the nonce calculation for a specified nonce value, can only be called on new blocks
 };
 
 
