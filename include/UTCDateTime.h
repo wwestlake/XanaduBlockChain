8fb9c43987def8f0e6874c496ccfa1e8a6363973
wwestlake <wwestlake@lagdaemon.com>
2018-08-23 11:24:11 -0400

    Update UTCDateTime.h

--- include/UTCDateTime.h
+++ include/UTCDateTime.h
@@ -1,8 +1,7 @@
 #ifndef __UTCDATETIME_H
 #define __UTCDATETIME_H
 
-#include <iostream>
-#include <typeinfo>
+#include <string>
 #include <boost/program_options.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
 #include <boost/date_time/local_time_adjustor.hpp>
@@ -11,14 +10,24 @@
 
 class UTCDateTime {
 
+  std::string today;
+  
   UTCDateTime()
   {
-  
-  
+    	using boost::posix_time::ptime;
+	    using boost::posix_time::second_clock;
+	    using boost::posix_time::to_simple_string;
+	    using boost::gregorian::day_clock;
+	    this->today = to_simple_string(ptime(day_clock::universal_day(), second_clock::universal_time().time_of_day()));
   }
 
+  std::string toString()
+  {
+      return today;
+  }
 
-
+  
+  
 };
 
 
