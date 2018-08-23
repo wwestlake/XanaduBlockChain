0b6cf79cc4db3392971b7c28eb790ab5bbea1fa4
wwestlake <wwestlake@lagdaemon.com>
2018-08-23 11:19:28 -0400

    Create UTCDateTime.h

---
+++ include/UTCDateTime.h
@@ -0,0 +1,29 @@
+#ifndef __UTCDATETIME_H
+#define __UTCDATETIME_H
+
+#include <iostream>
+#include <typeinfo>
+#include <boost/program_options.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/date_time/local_time_adjustor.hpp>
+#include <boost/date_time/c_local_time_adjustor.hpp>
+
+
+class UTCDateTime {
+
+  UTCDateTime()
+  {
+  
+  
+  }
+
+
+
+};
+
+
+
+
+
+#endif
+
